31dfa17acafee81ffdfc3d3491134ba599f23dbc
rizlas <rizlas@users.noreply.github.com>
2024-09-13 09:14:34 +0200

    feat: backup with multiple gpg working

--- src/backup.sh
+++ src/backup.sh
@@ -8,25 +8,44 @@ export PGHOST="${POSTGRES_HOST}"
 export PGPORT="${POSTGRES_PORT}"
 export PGDATABASE="${POSTGRES_DB}"
 
-timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
+TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
+BACKUPS_DIR=backups
+ENCRYPTED_BACKUPS_DIR=encrypted_backups
+DUMP_FILENAME=${POSTGRES_DB}_${TIMESTAMP}.dump
 
-echo "Creating backup of $POSTGRES_DB database..."
-mkdir backups
-DUMP_FILE=${POSTGRES_DB}_${timestamp}.dump
-pg_dump --format=custom $PGDUMP_EXTRA_OPTS > backups/$DUMP_FILE
+echo "Creating backup of '$POSTGRES_DB' database..."
+mkdir -p $BACKUPS_DIR
+mkdir -p $ENCRYPTED_BACKUPS_DIR
+pg_dump --format=custom $PGDUMP_EXTRA_OPTS > $BACKUPS_DIR/$DUMP_FILENAME
 
-if [ -n "$PASSPHRASE" ]; then
-  echo "Encrypting backup..."
-  mkdir encrypted
-  rm -f db.dump.gpg
-  gpg --symmetric --batch --passphrase "$PASSPHRASE" --output encrypted/$DUMP_FILE.gpg backups/$DUMP_FILE
-  # rm db.dump
-  # local_file="db.dump.gpg"
-  # s3_uri="${s3_uri_base}.gpg"
+# Encrypt the backup based on provided environment variables
+if [ -n "${GPG_EMAILS:-}" ]; then
+  echo "Encrypting backup using gpg emails..."
+  for EMAIL in $(echo $GPG_EMAILS | tr "," "\n"); do
+    echo "Using $EMAIL"
+    OUTPUT_FILENAME=${DUMP_FILENAME}_${EMAIL}.gpg
+
+    # Fetch the public key using WKD
+    gpg --auto-key-locate clear,wkd --locate-keys "$EMAIL"
+
+    # Encrypt the dump file using the located public key
+    gpg --batch --encrypt --recipient "$EMAIL" --trust-model always --output $ENCRYPTED_BACKUPS_DIR/$OUTPUT_FILENAME $BACKUPS_DIR/$DUMP_FILENAME
+
+    echo "Backup encrypted for $EMAIL as $OUTPUT_FILENAME"
+  done
+elif [ -n "${PASSPHRASE:-}" ]; then
+  echo "Encrypting backup using passphrase..."
+  gpg --symmetric --batch --passphrase "$PASSPHRASE" --output $ENCRYPTED_BACKUPS_DIR/$DUMP_FILENAME.gpg $BACKUPS_DIR/$DUMP_FILENAME
 else
-  local_file="db.dump"
-  s3_uri="$s3_uri_base"
+  echo "No encryption specified. Skipping encryption step."
 fi
+
+
+  # local_file="db.dump.gpg"
+# else
+#   local_file="db.dump"
+#   s3_uri="$s3_uri_base"
+# fi
 
 # echo "Uploading backup to $S3_BUCKET..."
 # aws $aws_args s3 cp "$local_file" "$s3_uri"
