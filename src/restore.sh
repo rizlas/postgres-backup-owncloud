67c71276700b51c2130f94070564c539e10a15c0
rizlas <rizlas@users.noreply.github.com>
2024-09-23 17:34:41 +0200

    wip: restore script

--- src/restore.sh
+++ src/restore.sh
@@ -1,44 +1,48 @@
 #!/bin/sh
 
-set -u # `-e` omitted intentionally, but i can't remember why exactly :'(
-set -o pipefail
+set -euo pipefail
 
-source ./env.sh
+download_from_owncloud() {
+  echo "Downloading $LATEST_BACKUP"
+  curl -s -u "$OWNCLOUD_SHARE_ID:$OWNCLOUD_SHARE_PASSWORD" \
+      https://$OWNCLOUD_FQDN/public.php/webdav/$LATEST_BACKUP -o /tmp/$LATEST_BACKUP
+}
 
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}"
+XML_TEMP_FILE=$(mktemp)
 
-if [ -z "$PASSPHRASE" ]; then
-  file_type=".dump"
+# List files
+curl -s -X PROPFIND -u "$OWNCLOUD_SHARE_ID:$OWNCLOUD_SHARE_PASSWORD" \
+    https://$OWNCLOUD_FQDN/public.php/webdav -o $XML_TEMP_FILE
+
+if [ -n "${RESTORE_GPG_EMAIL:-}" ]; then
+  echo "Getting latest backup crypted with $RESTORE_GPG_EMAIL gpg key..."
+  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE" --user-email "$RESTORE_GPG_EMAIL")
+  download_from_owncloud
+elif [ -n "${PASSPHRASE:-}" ]; then
+  echo "Getting latest backup crypted with passphrase..."
+  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE" --passphrase-crypted)
+  download_from_owncloud
+  gpg --batch --passphrase "$PASSPHRASE" --output /tmp/${LATEST_BACKUP%.gpg} --decrypt /tmp/$LATEST_BACKUP
 else
-  file_type=".dump.gpg"
+  echo "Getting latest backup without encryption..."
+  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE")
+  download_from_owncloud
 fi
 
-if [ $# -eq 1 ]; then
-  timestamp="$1"
-  key_suffix="${POSTGRES_DB}_${timestamp}${file_type}"
-else
-  echo "Finding latest backup..."
-  key_suffix=$(
-    aws $aws_args s3 ls "${s3_uri_base}/${POSTGRES_DB}" \
-      | sort \
-      | tail -n 1 \
-      | awk '{ print $4 }'
-  )
-fi
 
-echo "Fetching backup from S3..."
-aws $aws_args s3 cp "${s3_uri_base}/${key_suffix}" "db${file_type}"
+# rm /tmp/*.dump*
+# rm /tmp/tmp.*
 
-if [ -n "$PASSPHRASE" ]; then
-  echo "Decrypting backup..."
-  gpg --decrypt --batch --passphrase "$PASSPHRASE" db.dump.gpg > db.dump
-  rm db.dump.gpg
-fi
+# if [ -n "$PASSPHRASE" ]; then
+#   echo "Decrypting backup..."
+#   gpg --decrypt --batch --passphrase "$PASSPHRASE" db.dump.gpg > db.dump
+#   rm db.dump.gpg
+# fi
 
-conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB"
+# conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB"
 
-echo "Restoring from backup..."
-pg_restore $conn_opts --clean --if-exists db.dump
-rm db.dump
+# echo "Restoring from backup..."
+# pg_restore $conn_opts --clean --if-exists db.dump
+# rm db.dump
 
-echo "Restore complete."
+# echo "Restore complete."
