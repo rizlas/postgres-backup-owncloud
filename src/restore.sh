61c4817ca3d98a4622037f05bac05fd72f96e7a9
rizlas <rizlas@users.noreply.github.com>
2024-09-27 11:22:35 +0200

    feat: working mode. Restore backup either from remote owncloud instance or local owncloud folder

--- src/restore.sh
+++ src/restore.sh
@@ -8,7 +8,35 @@ cleanup() {
   rm -f /tmp/tmp.*
 }
 
-download_from_owncloud() {
+trap cleanup EXIT
+source ./env.sh
+
+echo -e "Restoring backup using $MODE mode!\n"
+
+if [ "$MODE" = "remote" ]; then
+  XML_TEMP_FILE=$(mktemp)
+  SOURCE="--xml-file $XML_TEMP_FILE"
+
+  # List files
+  curl -s -X PROPFIND -u "$OWNCLOUD_SHARE_ID:$OWNCLOUD_SHARE_PASSWORD" \
+      https://$OWNCLOUD_FQDN/public.php/webdav -o $XML_TEMP_FILE
+elif [ "$MODE" = "local" ]; then
+  SOURCE="--share-path $SHARE_PATH"
+fi
+
+if [ -n "${GPG_RESTORE_EMAIL:-}" ]; then
+  echo "Getting latest backup crypted with $GPG_RESTORE_EMAIL gpg key"
+  LATEST_BACKUP=$(python3 "get_backups.py" latest $SOURCE --database-name $POSTGRES_DB --user-email "$GPG_RESTORE_EMAIL")
+elif [ -n "${PASSPHRASE:-}" ]; then
+  echo "Getting latest backup crypted with passphrase"
+  LATEST_BACKUP=$(python3 "get_backups.py" latest $SOURCE --database-name $POSTGRES_DB --passphrase-crypted)
+else
+  echo "Getting latest backup without encryption"
+  LATEST_BACKUP=$(python3 "get_backups.py" latest $SOURCE --database-name $POSTGRES_DB)
+fi
+
+if [ "$MODE" = "remote" ]; then
+  # Download from ownCloud
   if [ -z "${LATEST_BACKUP:-}" ]; then
     echo "No backup file specified to download. Skipping download."
     return 1
@@ -22,43 +50,35 @@ download_from_owncloud() {
     echo "Error: Failed to download $LATEST_BACKUP."
     exit 1
   fi
-}
+fi
 
-trap cleanup EXIT
-source ./env.sh
-
-XML_TEMP_FILE=$(mktemp)
-
-# List files
-curl -s -X PROPFIND -u "$OWNCLOUD_SHARE_ID:$OWNCLOUD_SHARE_PASSWORD" \
-    https://$OWNCLOUD_FQDN/public.php/webdav -o $XML_TEMP_FILE
+# In case of mode is set to local, copy backup to /tmp
+# Avoid use the original file
+# Avoid to mess with path between mode remote and mode local when gpg or pg_restore
+# need to retrieve the file
+if [ "$MODE" = "local" ]; then
+  cp $SHARE_PATH/$LATEST_BACKUP /tmp/$LATEST_BACKUP
+fi
 
 if [ -n "${GPG_RESTORE_EMAIL:-}" ]; then
-  echo "Getting latest backup crypted with $GPG_RESTORE_EMAIL gpg key..."
-  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE" --database-name $POSTGRES_DB --user-email "$GPG_RESTORE_EMAIL")
-  download_from_owncloud
-  # You must have the recipient key that was used to encrypt
+  echo "Decrypting $LATEST_BACKUP"
+  # Check if key passphrase exist and if it is, disable interactive mode
   KEY_PASSHPRASE=""
   if [ -n "${GPG_RESTORE_EMAIL_PASSPHRASE:-}" ]; then
     KEY_PASSHPRASE="--pinentry-mode=loopback --passphrase $GPG_RESTORE_EMAIL_PASSPHRASE"
   fi
+  # You must have the recipient key that was used to encrypt
   gpg --batch --yes $KEY_PASSHPRASE --output /tmp/${LATEST_BACKUP%.gpg} --decrypt /tmp/$LATEST_BACKUP
 elif [ -n "${PASSPHRASE:-}" ]; then
-  echo "Getting latest backup crypted with passphrase..."
-  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE" --database-name $POSTGRES_DB --passphrase-crypted)
-  download_from_owncloud
+  echo "Decrypting $LATEST_BACKUP"
   gpg --batch --passphrase "$PASSPHRASE" --output /tmp/${LATEST_BACKUP%.gpg} --decrypt /tmp/$LATEST_BACKUP
-else
-  echo "Getting latest backup without encryption..."
-  LATEST_BACKUP=$(python3 "parse_xml.py" latest "$XML_TEMP_FILE" --database-name $POSTGRES_DB)
-  download_from_owncloud
 fi
 
+echo "Restoring from: /tmp/${LATEST_BACKUP%.gpg}"
+
 if [ "$DRY_RUN" = true ]; then
-  echo "File to restore: /tmp/${LATEST_BACKUP%.gpg}"
-  echo "Dry run mode: Skipping actual restore."
+  echo "Dry run mode: skipping actual restore!"
 else
-  echo "Restoring from backup..."
   pg_restore --clean --if-exists -d $PGDATABASE /tmp/${LATEST_BACKUP%.gpg}
   echo "Restore complete!"
 fi
